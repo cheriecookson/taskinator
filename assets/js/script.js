8114efa4bc4fedc2b722c0ad5d919419c098bcca
Cherie Cookson <cheriecookson@gmail.com>
2020-06-10 23:00:03 -0500

    added alert to notify changes.  Fixed other error

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -16,8 +18,21 @@ var taskFormHandler = function(event) {
 
     formEl.reset();
 
+    var completeEditTask = function(taskName, taskType, taskId) {
+      // find the matching task list item
+      var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+      // set new values
+      taskSelected.querySelector("h3.task-name").textContent = taskName;
+      taskSelected.querySelector("span.task-type").textContent = taskType;
+
+      alert("Task Updated!");
+      formEl.removeAttribute("data-task-id");
+      document.querySelector("#save-task").textContent = "Add Task";
+      
+    };
    
-    var isEdit = formEl.hasAttribute("data-task-id");   
+var isEdit = formEl.hasAttribute("data-task-id");   
     // has data attribute, so get task id and call function to complete edit process
     if (isEdit) {
     var taskId = formEl.getAttribute("data-task-id");
@@ -32,7 +47,7 @@ var taskFormHandler = function(event) {
   
     createTaskEl(taskDataObj);
   }
-
+}
   var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -96,8 +111,31 @@ var taskFormHandler = function(event) {
 
   };
 
+  var taskStatusChangeHandler = function(event) {
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+  
+    // get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+  
+    // find the parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    if (statusValue === "to do") {
+      tasksToDoEl.appendChild(taskSelected);
+    } 
+    else if (statusValue === "in progress") {
+      tasksInProgressEl.appendChild(taskSelected);
+    } 
+    else if (statusValue === "completed") {
+      tasksCompletedEl.appendChild(taskSelected);
+    }
+
+
+  };
+
+
+
   var editTask = function(taskId) {
-    console.log("editing task #" + taskId);
   
     // get task list item element
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
@@ -109,7 +147,6 @@ var taskFormHandler = function(event) {
     document.querySelector("select[name='task-type']").value = taskType;
     document.querySelector("#save-task").textContent = "Save Task";
     formEl.setAttribute("data-task-id", taskId);
-    
   };
 
   var deleteTask = function(taskId) {
@@ -135,3 +172,4 @@ var taskFormHandler = function(event) {
     }
   };
   pageContentEl.addEventListener("click", taskButtonHandler);
+  pageContentEl.addEventListener("change", taskStatusChangeHandler);
